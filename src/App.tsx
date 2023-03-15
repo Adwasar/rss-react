98bedc8584253b57cc72c618787923274bc400ca
Adwasar <user@Users-MacBook-Pro.local>
2023-03-15 18:02:45 +0200

    feat: add sass preprocessor

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 import { HomePage } from './pages/HomePage';
-import { BlogPage } from './pages/BlogPage';
 import { AboutPage } from './pages/AboutPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <>
       <header>
         <NavLink to="/">Home</NavLink>
-        <NavLink to="/posts">Blog</NavLink>
-        <NavLink to="/about">About</NavLink>
+        <NavLink to="/about">About Us</NavLink>
       </header>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/posts" element={<BlogPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
