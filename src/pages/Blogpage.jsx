70fa0ba74cef9202c6dbede8da5063e6b1882a6b
Adwasar <user@Users-MacBook-Pro.local>
2023-03-15 11:49:44 +0200

    feat: add sample pages

---
+++ src/pages/Blogpage.jsx
@@ -0,0 +1,10 @@
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Blog content</p>
+    </div>
+  );
+};
+
+export { Blog };
---
+++ src/pages/Notfoundpage.jsx
@@ -0,0 +1,10 @@
+const Notfound = () => {
+  return (
+    <div>
+      <h1>Not found</h1>
+      <p>This page not found</p>
+    </div>
+  );
+};
+
+export { Notfound };
