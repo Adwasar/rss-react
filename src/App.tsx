c7298879be08af914900ceaadd2b84725d8ca328
Adwasar <user@Users-MacBook-Pro.local>
2023-03-16 16:50:38 +0200

    feat: add data of cafds

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,11 @@ function App() {
         <NavLink to="/about">About Us</NavLink>
       </header>
       <main>
-        <div className={'filter-board'}>
-          <div className="wrapper">
-            <div className={'search-wrapper'}>
-              <input type="text" />
-              <button>Search</button>
-            </div>
-          </div>
-        </div>
-        <div className="wrapper">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </main>
     </>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,19 @@
 const HomePage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <p>Homepage content</p>
-    </div>
+    <>
+      <div className={'filter-board'}>
+        <div className="wrapper">
+          <div className={'search-wrapper'}>
+            <input type="text" />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+      <div className="wrapper">
+        <h1>Homepage</h1>
+        <p>Homepage content</p>
+      </div>
+    </>
   );
 };
 
