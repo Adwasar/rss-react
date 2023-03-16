04d997fe14824679ef215892fbde57d3af22ac15
Adwasar <user@Users-MacBook-Pro.local>
2023-03-16 16:29:15 +0200

    feat: add search baar layout

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,23 @@ function App() {
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About Us</NavLink>
       </header>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <main>
+        <div className={'filter-board'}>
+          <div className="wrapper">
+            <div className={'search-wrapper'}>
+              <input type="text" />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+        <div className="wrapper">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </main>
     </>
   );
 }
