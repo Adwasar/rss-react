b5800ac3e217fdce8d2ab6c49b1cb89b607a8042
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-03-25 13:22:38 +0200

    refactor: FormPage as class component

--- src/pages/FormPage/index.tsx
+++ src/pages/FormPage/index.tsx
@@ -1,17 +1,27 @@
+import React from 'react';
+
 import { Header } from '../../components/Header/Header';
 
 import { NavTitle } from '../../types/data-types';
 
-const FormPage = (props: NavTitle) => {
-  return (
-    <>
-      <Header title={props.title} />
-      <div className="wrapper">
-        <h1>Form</h1>
-        <p>This will be form</p>
-      </div>
-    </>
-  );
-};
+class FormPage extends React.Component<NavTitle> {
+  render() {
+    return (
+      <>
+        <Header title={this.props.title} />
+        <div className="wrapper">
+          <h1>Form</h1>
+          <form>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" />
+            <br />
+            <label htmlFor="surname">Surname:</label>
+            <input type="text" id="surname" />
+          </form>
+        </div>
+      </>
+    );
+  }
+}
 
 export { FormPage };
