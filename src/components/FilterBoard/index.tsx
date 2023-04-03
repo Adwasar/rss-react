933e6f99d8097d6f7f26c80a3b696c47d087230b
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-04-03 13:34:31 +0300

    feat: localStorage for inputFind

--- src/components/FilterBoard/index.tsx
+++ src/components/FilterBoard/index.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+
 import styles from './FilterBoard.module.scss';
 
 export const FilterBoard = () => {
+  const [inputFind, setInputFind] = useState('');
+
+  const handleInputFindSubmit = () => {
+    setInputFind('');
+    localStorage.setItem('inputFind', inputFind);
+  };
+
   return (
     <div className={styles['filter-board']}>
       <div className="wrapper">
         <div className={styles['search-wrapper']}>
-          <input className={styles['search-input']} type="text" />
-          <button className={styles['search-btn']}>
+          <input
+            className={styles['search-input']}
+            value={inputFind}
+            onChange={(e) => setInputFind(e.target.value)}
+            type="text"
+          />
+          <button className={styles['search-btn']} onClick={handleInputFindSubmit}>
             <img src="./icons/find.svg" alt="#" className={styles.test} />
           </button>
         </div>
