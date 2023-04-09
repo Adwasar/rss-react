2a1dda76fa93e7c74f355ceb9bdcb77e89c3a27c
Vladislav <adwasar@Vladislavs-MacBook-Pro.local>
2023-04-09 17:28:47 +0300

    feat: loader for find input change

--- src/components/FilterBoard/index.tsx
+++ src/components/FilterBoard/index.tsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 import { Data } from '../../types/data-types';
 
+import { Loader } from '../Loader';
+
 import styles from './FilterBoard.module.scss';
 
 export const FilterBoard = (props: { onFilterResult: (value: Data) => void }) => {
   const [inputFind, setInputFind] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const savedValue = localStorage.getItem('currentInputFindValue');
@@ -15,11 +18,15 @@ export const FilterBoard = (props: { onFilterResult: (value: Data) => void }) =>
   }, []);
 
   useEffect(() => {
-    fetch(`https://rickandmortyapi.com/api/character/?name=${inputFind}`)
-      .then((response) => response.json())
-      .then((data) => props.onFilterResult(data))
-      .catch((error) => console.error(error));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    setIsLoading(true);
+    setTimeout(() => {
+      fetch(`https://rickandmortyapi.com/api/character/?name=${inputFind}`)
+        .then((response) => response.json())
+        .then((data) => props.onFilterResult(data))
+        .catch((error) => console.error(error))
+        .finally(() => setIsLoading(false));
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, 200);
   }, [inputFind]);
 
   const handleInputFindChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,20 +41,23 @@ export const FilterBoard = (props: { onFilterResult: (value: Data) => void }) =>
   };
 
   return (
-    <div className={styles['filter-board']}>
-      <div className="wrapper">
-        <div className={styles['search-wrapper']}>
-          <input
-            className={styles['search-input']}
-            value={inputFind}
-            onChange={handleInputFindChange}
-            type="text"
-          />
-          <button className={styles['search-btn']} onClick={handleInputFindSubmit}>
-            <img src="./icons/find.svg" alt="#" className={styles.test} />
-          </button>
+    <>
+      <div className={styles['filter-board']}>
+        <div className="wrapper">
+          <div className={styles['search-wrapper']}>
+            <input
+              className={styles['search-input']}
+              value={inputFind}
+              onChange={handleInputFindChange}
+              type="text"
+            />
+            <button className={styles['search-btn']} onClick={handleInputFindSubmit}>
+              <img src="./icons/find.svg" alt="#" className={styles.test} />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {isLoading && <Loader />}
+    </>
   );
 };
